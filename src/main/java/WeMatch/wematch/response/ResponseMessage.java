e144e5a53c15636a88df520bf89db7a284a2f64a
seungyeon <jmy9937@sookmyung.ac.kr>
2023-07-31 15:52:31 +0900

    feat : responseMessage 추가

--- src/main/java/WeMatch/wematch/response/ResponseMessage.java
+++ src/main/java/WeMatch/wematch/response/ResponseMessage.java
@@ -2,10 +2,15 @@ package WeMatch.wematch.response;
 
 public class ResponseMessage {
 
+    public static final String SUCCESS = "성공";
     public static final String SUCCESS_SIGN_UP = "회원가입 성공";
     public static final String SUCCESS_SIGN_IN="로그인 성공";
     public static final String SUCCESS_LOG_OUT="로그아웃 성공";
     public static final String SUCCESS_TODO_SAVE="Todo 생성 성공";
     public static final String SUCCESS_TODO_UPDATE="Todo 수정 성공";
+    public static final String SUCCESS_TODO_CHECK="Todo 완료 변경 성공";
     public static final String SUCCESS_TODO_DELETE="Todo 삭제 성공";
+    public static final String SUCCESS_EVENT_SAVE="Event 생성 성공";
+    public static final String SUCCESS_EVENT_UPDATE="Event 수정 성공";
+    public static final String SUCCESS_EVENT_DELETE="Event 삭제 성공";
 }
