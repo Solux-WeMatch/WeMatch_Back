8659e4f59fd6c2bc96c1fbf452c7031491d6995d
Seungyeon Kwon <114494828+tmdduuus@users.noreply.github.com>
2023-08-08 03:23:00 +0900

    feat : WebConfig 추가

---
+++ src/main/java/WeMatch/wematch/config/security/WebConfig.java
@@ -0,0 +1,22 @@
+package WeMatch.wematch.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173") // 허용할 Origin 설정
+                .allowedOrigins("http://localhost:5174")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메소드 설정
+                .allowedHeaders("*") // 허용할 Header 설정
+                .allowCredentials(true); // 쿠키 사용 허용 여부 설정
+    }
+}
