198fd540c473ebbe8c6c94fa874dc61fcd4d5297
Seungyeon Kwon <114494828+tmdduuus@users.noreply.github.com>
2023-08-08 23:00:40 +0900

    fix

--- src/main/java/WeMatch/wematch/domain/member/dto/TeamCreateRequestDto.java
+++ src/main/java/WeMatch/wematch/domain/member/dto/TeamCreateRequestDto.java
@@ -9,13 +9,10 @@ import lombok.Setter;
 @NoArgsConstructor
 public class TeamCreateRequestDto {
     private Long memberId;
-    private Long teamId;
     private String teamName;
 
-    public TeamCreateRequestDto(Long memberId, Long teamId, String teamName) {
+    public TeamCreateRequestDto(Long memberId, String teamName) {
         this.memberId = memberId;
-        this.teamId = teamId;
         this.teamName = teamName;
     }
-
 }
