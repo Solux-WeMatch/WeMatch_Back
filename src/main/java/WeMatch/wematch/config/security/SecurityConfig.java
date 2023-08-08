b628b3e22ad636562003a709cc0a86e84d3de06f
Seungyeon Kwon <114494828+tmdduuus@users.noreply.github.com>
2023-08-09 02:44:23 +0900

    fix : security add

---
+++ src/main/java/WeMatch/wematch/config/security/SecurityConfig.java
@@ -0,0 +1,14 @@
+package WeMatch.wematch.config.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .anyRequest().permitAll(); // 모든 요청에 대해 권한 검사를 하지 않음
+    }
+}
