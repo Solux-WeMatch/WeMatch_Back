1413f34b70f832b5dd47f82a40aaeeadb6b96174
Seungyeon Kwon <114494828+tmdduuus@users.noreply.github.com>
2023-08-09 10:12:40 +0900

    fix : cors policy

--- src/main/java/WeMatch/wematch/config/security/WebConfig.java
+++ src/main/java/WeMatch/wematch/config/security/WebConfig.java
@@ -1,25 +1,22 @@
 package WeMatch.wematch.config.security;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-@EnableWebSecurity
-public class WebConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-    }
+public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*");
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173") // 허용할 Origin 설정
+                .allowedOrigins("http://localhost:5174")
+                .allowedOrigins("http://localhost:8080")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH") // 허용할 HTTP 메소드 설정
+                .allowedHeaders("*") // 허용할 Header 설정
+                .allowCredentials(true); // 쿠키 사용 허용 여부 설정
     }
 }
